74761ff89bf8ebf95d24422f005708f5d3a3b23c
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-04 10:19:01 -0700

    Create function divide()

--- main.js
+++ main.js
@@ -7,5 +7,9 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return (num1 * num2).toPrecision(8);
+}
+
+function divide(num1, num2) {
+  return (num1 / num2).toPrecision(8);
 }
